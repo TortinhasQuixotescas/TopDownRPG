c11aba1398be31022c43c5272392709895be54c8
Lucas Paiva <lucaspaiva@live.com>
2023-10-25 14:46:14 -0300

    corrige bug dos múltiplos jogadores

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D playerRB;
-
+    public int moveSpeed = 5;
+    public Animator playerAnimator;
+    public static PlayerController uniqueInstance;
     // Start is called before the first frame update
     void Start()
-    {
 
+    {
+        if (uniqueInstance == null)
+            uniqueInstance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        playerAnimator.SetFloat("xMovement", playerRB.velocity.x);
+        playerAnimator.SetFloat("yMovement", playerRB.velocity.y);
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            playerAnimator.SetFloat("lastXValue", Input.GetAxisRaw("Horizontal"));
+            playerAnimator.SetFloat("lastYValue", Input.GetAxisRaw("Vertical"));
+        }
     }
 }
