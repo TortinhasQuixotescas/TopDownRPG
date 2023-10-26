422e8f6f00813ce0a4d75f12668abc50197b221d
Lucas Paiva <lucaspaiva@live.com>
2023-10-25 22:04:06 -0300

    adiciona verificações e tratamentos de exceções para transições de cenas

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerController.uniqueInstance == null)
+            Instantiate(player);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
