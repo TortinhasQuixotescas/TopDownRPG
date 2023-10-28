0bee226446f0f985276bf52125bcedd808412394
Lucas Paiva <lucaspaiva@live.com>
2023-10-28 06:22:22 -0300

    adiciona animações de ataque

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
     public int moveSpeed = 5;
     public Animator playerAnimator;
     public string areaTransitionName;
+    public bool busy = false;
+
+    private float lastXValue = 0;
+    private float lastYValue = 0;
     // Start is called before the first frame update
     void Start()
 
@@ -24,13 +28,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
-        playerAnimator.SetFloat("xMovement", playerRB.velocity.x);
-        playerAnimator.SetFloat("yMovement", playerRB.velocity.y);
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if (!busy)
         {
-            playerAnimator.SetFloat("lastXValue", Input.GetAxisRaw("Horizontal"));
-            playerAnimator.SetFloat("lastYValue", Input.GetAxisRaw("Vertical"));
+            playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+            playerAnimator.SetFloat("xMovement", playerRB.velocity.x);
+            playerAnimator.SetFloat("yMovement", playerRB.velocity.y);
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                lastXValue = Input.GetAxisRaw("Horizontal");
+                playerAnimator.SetFloat("lastXValue", Input.GetAxisRaw("Horizontal"));
+                lastYValue = Input.GetAxisRaw("Vertical");
+                playerAnimator.SetFloat("lastYValue", Input.GetAxisRaw("Vertical"));
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            busy = true;
+            if (lastXValue > 0)
+                playerAnimator.SetTrigger("swordAttackEast");
+            else if (lastXValue < 0)
+                playerAnimator.SetTrigger("swordAttackWest");
+            else if (lastYValue > 0)
+                playerAnimator.SetTrigger("swordAttackNorth");
+            else if (lastYValue < 0)
+                playerAnimator.SetTrigger("swordAttackSouth");
         }
     }
 }
