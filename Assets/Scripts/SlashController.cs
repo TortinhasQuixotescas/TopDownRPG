6b4a02222592e0a2503658d265a1537b3fe34259
Gabriel Malosto <professional@gabdumal.com>
2023-10-29 04:32:25 -0300

    Simplifica Slash Controller

---
+++ Assets/Scripts/SlashController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashController : MonoBehaviour
+{
+
+    public float xPositionOffset;
+    public float yPositionOffset;
+    private Transform player;
+
+    void Start()
+    {
+        this.SetItUp();
+    }
+
+    void Update()
+    {
+        this.UpdateCurrentPosition();
+    }
+
+    void OnEnable()
+    {
+        this.SetItUp();
+    }
+
+    protected void SetItUp()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        this.UpdateCurrentPosition();
+        StartCoroutine(DelayExecution(0.30f));
+    }
+    private void UpdateCurrentPosition()
+    {
+        Vector2 position = new Vector2(
+            player.position.x + this.xPositionOffset,
+            player.position.y + this.yPositionOffset
+        );
+        this.transform.position = position;
+    }
+
+    private IEnumerator DelayExecution(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        gameObject.SetActive(false);
+    }
+
+}
